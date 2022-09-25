c2021722e23e2bb52c4658246918c02b19f3ea75
jshortall11 <jshortall@student.bridgew.edu>
2022-09-24 21:26:10 -0400

    Initial commit

---
+++ project1JamesShortall.py
@@ -0,0 +1,8 @@
+print("What year were you born")
+your_birthyear= input(2004)
+print("What is your name")
+your_name= input("James Shortall")
+print("How many years ago were you born")
+your_age= input(2022-2004)
+comment_to_user = f"My name is {your_name} and im turning {your_age} years old this year"
+print(comment_to_user)
